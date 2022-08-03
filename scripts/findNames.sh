30f92d8a58cb76009f9b22ba69634e7aedcac326
Ulises Nemeth <ulisesnemeth@gmail.com>
2022-08-03 04:11:45 -0300

    Se avanzo con el script findNames.sh y se modifico un archivo de texto para probar mejor este script

--- scripts/findNames.sh
+++ scripts/findNames.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo findNames.sh
+echo Listado de nombres propios dentro del texto
+#Se itera por cada palabra que tenga una letra mayuscula, para luego verificar si comienza con la letra mayuscula
+for NOMBREPROPIO in $(grep -o "[^ ]*[A-Z][^ ]*" $1)
+do
+    if [[ $NOMBREPROPIO =~ ^[A-Z] ]] #Falta contemplar las palabras que tengan varias mayusculas
+    then
+    echo $NOMBREPROPIO
+    fi
+done
 exit 0
