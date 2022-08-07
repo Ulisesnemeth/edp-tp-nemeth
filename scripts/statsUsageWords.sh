9f2a62b37a162b2f129fe4c9df26aeb576ecbe62
Ulises Nemeth <ulisesnemeth@gmail.com>
2022-08-07 02:25:12 -0300

    Script statsUsageWords.sh terminado

--- scripts/statsUsageWords.sh
+++ scripts/statsUsageWords.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-echo statsUsageWords.sh
+PALABRAS=""
+for PALABRA in $(cat $1 | tr -d [:punct:])
+do
+    if [[ ${#PALABRA} > 3 ]]
+    then
+        PALABRAS=${PALABRAS}" "${PALABRA^}
+    fi
+done
+CONTADOR=0
+echo Top de palabras mas usadas:
+for i in $(sort <<< $(grep -o "[^ ]*[^ ]*" <<< $PALABRAS) | uniq -c)
+do
+    CONTADOR=$(($CONTADOR+1))
+    if [ $CONTADOR -lt 21 ]&& [ $(($CONTADOR%2)) -eq 0 ]
+    then
+        echo $i
+    fi
+done
 exit 0
