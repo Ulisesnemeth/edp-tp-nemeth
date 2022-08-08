b54cbd8b9657c8daebe531af2642def884b69560
Ulises Nemeth <ulisesnemeth@gmail.com>
2022-08-08 18:17:43 -0300

    Se arreglo un error que surgía porque no se contemplaron las palabras de una letra

--- scripts/findNames.sh
+++ scripts/findNames.sh
@@ -3,13 +3,16 @@ echo Listado de nombres propios dentro del texto
 #Se itera por cada palabra que tenga una letra mayuscula por lo menos, para luego verificar si comienza con la letra mayuscula y el resto de sus letras están en minúscula
 for NOMBREPROPIO in $(grep -o "[^ ]*[A-Z][^ ]*" $1)
 do
-    RESTO=$(echo $NOMBREPROPIO | cut -c 2-${#NOMBREPROPIO})
-    PRIMERLETRA=$(echo $NOMBREPROPIO | cut -c 1-1)
-    if [[ $PRIMERLETRA =~ ^[A-Z] ]] #Se comprueba que la primer letra sea mayúscula
+    if [ ${#NOMBREPROPIO} -gt 1 ] #Se comprueba que el nombre tenga mas de 1 letra para poder cumplir con el formato
     then
-        if [[ ! $RESTO =~ [A-Z] ]] #Se comprueba que no existan otras mayusculas
+        RESTO=$(echo $NOMBREPROPIO | cut -c 2-${#NOMBREPROPIO})
+        PRIMERLETRA=$(echo $NOMBREPROPIO | cut -c 1-1)
+        if [[ $PRIMERLETRA =~ ^[A-Z] ]] #Se comprueba que la primer letra sea mayúscula
         then
-        echo $NOMBREPROPIO
+            if [[ ! $RESTO =~ [A-Z] ]] #Se comprueba que no existan otras mayusculas
+            then
+            echo $NOMBREPROPIO
+            fi
         fi
     fi
 done
